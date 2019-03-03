84f5c1a5e1ea0841ea875537c08f03c3e7f06e4d
Steven Straker <contact@atrakez.com>
2019-03-02 20:00:33 -0500

    Add controller and factory for generated name

---
+++ app/Http/Controllers/GeneratedNameController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\GeneratedName;
+use Illuminate\Http\Request;
+
+class GeneratedNameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\GeneratedName  $generatedName
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GeneratedName $generatedName)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\GeneratedName  $generatedName
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GeneratedName $generatedName)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\GeneratedName  $generatedName
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GeneratedName $generatedName)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\GeneratedName  $generatedName
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GeneratedName $generatedName)
+    {
+        //
+    }
+}
